b645eb56a74da29d0a5e5ab3c496e49a9fe23788
rajanand02 <raj_anand02@yahoo.in>
2014-01-20 18:12:36 +0530

    replace static posts

--- client/views/posts/post_list.js
+++ client/views/posts/post_list.js
@@ -1,21 +1,5 @@
-var postsData = [
-  {
-  title: 'Introducing Telescope',
-  author: 'Sacha Greif',
-  url: 'http://sachagreif.com/introducing-telescope/'
-},
-{
-  title: 'Meteor',
-  author: 'Tom Coleman',
-  url: 'http://meteor.com'
-},
-{
-  title: 'Rails Tutorial',
-  author: 'Micheal hartl',
-  url: 'http://ruby.railstutorial.org/'
-}
-];
-
 Template.postsList.helpers({
-  posts: postsData
+  posts: function(){
+    return Posts.find();
+  }
 });
