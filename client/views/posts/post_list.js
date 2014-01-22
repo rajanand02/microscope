ca16391b5517ffe2b2888f6afc66821fa24f0453
rajanand02 <raj_anand02@yahoo.in>
2014-01-22 21:01:08 +0530

    add voting links

--- client/views/posts/post_list.js
+++ client/views/posts/post_list.js
@@ -1,5 +1,13 @@
 Template.postsList.helpers({
-  posts: function(){
-    return Posts.find({}, { sort: {submitted: -1}});
+  postsWithRank: function() {
+    this.posts.rewind();
+    return this.posts.map(function(post, index, cursor) {
+      post._rank = index;
+      return post;
+    });
+  },
+  hasMorePosts: function(){
+    this.posts.rewind();
+    return Router.current().limit() == this.posts.fetch().length;
   }
 });
