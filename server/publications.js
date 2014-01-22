a127f8439e821acd2a0697dd2a79d4b7abce4e50
rajanand02 <raj_anand02@yahoo.in>
2014-01-22 20:58:28 +0530

    make code readable

--- server/publications.js
+++ server/publications.js
@@ -1,9 +1,9 @@
-Meteor.publish('posts', function(){
-  return Posts.find();
+Meteor.publish('posts', function(options) {
+  return Posts.find({}, options);
 });
-Meteor.publish('comments', function(postId){
+Meteor.publish('comments', function(postId) {
   return Comments.find({postId: postId});
 });
-Meteor.publish('notifications', function () {
+Meteor.publish('notifications', function() {
   return Notifications.find({userId: this.userId});
 });
