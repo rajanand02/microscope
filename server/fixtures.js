1f0b9fa0844c3193b3dff32c270314d911fd39b1
rajanand02 <raj_anand02@yahoo.in>
2014-01-23 10:07:18 +0530

    add voting and router controllers

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,6 +1,7 @@
-// Fixture data
+// Fixture data 
 if (Posts.find().count() === 0) {
   var now = new Date().getTime();
+  
   // create two users
   var tomId = Meteor.users.insert({
     profile: { name: 'Tom Coleman' }
@@ -9,14 +10,18 @@ if (Posts.find().count() === 0) {
   var sachaId = Meteor.users.insert({
     profile: { name: 'Sacha Greif' }
   });
-  var sacha = Meteor.users.findOne(sachaId);var telescopeId = Posts.insert({
+  var sacha = Meteor.users.findOne(sachaId);
+  
+  var telescopeId = Posts.insert({
     title: 'Introducing Telescope',
     userId: sacha._id,
     author: sacha.profile.name,
     url: 'http://sachagreif.com/introducing-telescope/',
     submitted: now - 7 * 3600 * 1000,
-    commentsCount: 2
+    commentsCount: 2,
+    upvoters: [], votes: 0
   });
+  
   Comments.insert({
     postId: telescopeId,
     userId: tom._id,
@@ -24,6 +29,7 @@ if (Posts.find().count() === 0) {
     submitted: now - 5 * 3600 * 1000,
     body: 'Interesting project Sacha, can I get involved?'
   });
+  
   Comments.insert({
     postId: telescopeId,
     userId: sacha._id,
@@ -31,29 +37,36 @@ if (Posts.find().count() === 0) {
     submitted: now - 3 * 3600 * 1000,
     body: 'You sure can Tom!'
   });
+  
   Posts.insert({
     title: 'Meteor',
     userId: tom._id,
     author: tom.profile.name,
     url: 'http://meteor.com',
     submitted: now - 10 * 3600 * 1000,
-    commentsCount: 4
+    commentsCount: 0,
+    upvoters: [], votes: 0
   });
+  
   Posts.insert({
     title: 'The Meteor Book',
     userId: tom._id,
     author: tom.profile.name,
     url: 'http://themeteorbook.com',
     submitted: now - 12 * 3600 * 1000,
-    commentsCount: 7
+    commentsCount: 0,
+    upvoters: [], votes: 0
   });
-  for (var i = 0; i < 10; i ++) {
+  
+  for (var i = 0; i < 10; i++) {
     Posts.insert({
-      title: 'test post #' + i,
+      title: 'Test post #' + i,
       author: sacha.profile.name,
+      userId: sacha._id,
       url: 'http://google.com/?q=test-' + i,
-      submitted: now - i * 3600 * 1000,
-      commentsCount: 0
+      submitted: now - i * 3600 * 1000 + 1,
+      commentsCount: 0,
+      upvoters: [], votes: 0
     });
   }
 }
